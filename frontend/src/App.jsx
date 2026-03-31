f46f52b729e7bc45dc0380fe4e798feba0c163a0
kumarayshu852 <kumarayshu852@gmail.com>
2026-03-31 14:08:21 +0530

    fix import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import TenantDashboard from "./pages/tenantDashboard";
  
